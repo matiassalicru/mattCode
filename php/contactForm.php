a55474768165b1f7afe91e6e42deb20fd1c7a0d5
Matt <mattiassalicru@gmail.com>
2020-03-03 21:25:18 -0300

    Ordenado

---
+++ php/contactForm.php
@@ -0,0 +1,16 @@
+<?php
+
+    $email = $_POST['email'];
+    $mensaje = $_POST['mensaje'];
+
+    //Datos de a quien se le envía y que se le envía
+    $mailTo = "mattiassalicru@gmail.com";
+    $headers = "From: ".$email;
+    $txt = "You have received an email form ".$email. ".\n\n" . "El mensaje es el siguiente: \n".$mensaje;
+
+    //Enviar el mensaje
+    mail($mailTo, $headers, $txt);
+    header("Location:index.html");
+
+
+?>
